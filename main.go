17600dd5b86d9fbdc1756f2b1c7d1d46cc6db737
Wanderson <wandersonsoared831@gmail.com>
2023-11-23 22:25:44 -0300

    feat: add Server mux

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/WandersonSoares00/Api-Rest-Golang.git/configs"
+	"github.com/WandersonSoares00/Api-Rest-Golang.git/routes"
+)
+
 func main() {
-	print("Iniciando go... done!\n")
+	logger := configs.GetLogger("main")
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/api/v1", http.HandlerFunc(routes.Serve))
+	mux.Handle("/api/v1/", http.HandlerFunc(routes.Serve))
+
+	p := configs.GetServerPort()
+
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", p))
+
+	if err != nil {
+		logger.Errorf("error starting server: %s\n", err.Error())
+		return
+	}
+
+	logger.Infof("server listening in port %d", p)
+
+	http.Serve(ln, mux)
+
 }
