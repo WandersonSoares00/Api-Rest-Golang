f13cdaca1239b4dfff36583696260375f79e1585
Wanderson Soares <wandersonsoares841@gmail.com>
2023-11-25 20:31:44 -0300

    feat: add database entities

--- schema/schema.go
+++ schema/schema.go
@@ -1,24 +1,60 @@
 package schema
 
+import "time"
+
+type Telefone_gravadora struct {
+	Cod    int    `json:"cod"`
+	Numero string `json:"numero"`
+}
+
 type Gravadora struct {
+	Cod     int    `json:"cod"`
+	CodFone int    `json:"codFone"`
+	Nome    string `json:"nome"`
+	Ender   string `json:"endereco"`
+	HomeP   string `json:"home page"`
 }
 
 type Album struct {
+	Cod    int       `json:"cod"`
+	Desc   string    `json:"descricao"`
+	DtGrav time.Time `json:"dtgrav"`
+	PrComp float32   `json:"prcompra"`
+	PrVen  float32   `json:"prvenda"`
 }
 
 type Composicao struct {
+	Cod  int    `json:"cod"`
+	Desc string `json:"descricao"`
+	Tipo string `json:"tipo"`
 }
 
 type Faixa struct {
+	Cod    int           `json:"cod"`
+	Num    int           `json:"numero"`
+	Desc   string        `json:"descricao"`
+	TExec  time.Duration `json:"tempexec"`
+	TpGrav string        `json:"tipogravacao"`
 }
 
 type Compositor struct {
+	Cod    int       `json:"cod"`
+	Nome   string    `json:"nome"`
+	DtNasc time.Time `json:"dtnascimento"`
+	DtMort time.Time `json:"dtmorte,omitempty"`
 }
 
 type Playlist struct {
+	Cod     int           `json:"cod"`
+	Nome    string        `json:"nome"`
+	TempTot time.Duration `json:"tempotot"`
+	DtCriac time.Duration `json:"dtcriacao"`
 }
 
 type Interprete struct {
+	Cod  int    `json:"cod"`
+	Nome string `json:"nome"`
+	Tipo string `json:"tipo"`
 }
 
 func GetTable(t string) interface{} {
