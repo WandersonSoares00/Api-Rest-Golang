6cf166947cf2a27871af04351fac022f92002ce3
Wanderson Soares <wandersonsoares841@gmail.com>
2023-12-03 23:17:28 -0300

    feat: add route for faixa_playlist

--- routes/routes.go
+++ routes/routes.go
@@ -20,7 +20,7 @@ func init() {
 func Serve(w http.ResponseWriter, r *http.Request) {
 
 	var entity string
-	var id int64
+	var id int
 	var err error = nil
 	p := r.URL.Path
 
@@ -34,32 +34,34 @@ func Serve(w http.ResponseWriter, r *http.Request) {
 			msg := fmt.Sprintf("Entidade %s requisitada não definida", entity)
 			handlers.ReturnJsonResponse(w, http.StatusBadRequest, handlers.MessageToJson(false, msg))
 		} else if r.Method == http.MethodGet {
-			err = t.GetAll(w)
+			err = t.Get(w)
+		} else if r.Method == http.MethodDelete {
+			err = t.Delete(w, r)
 		} else if r.Method == http.MethodPost {
 			err = t.Create(w, r)
 		} else {
 			msg := "Método e requisição solicitados são incompatíveis."
 			handlers.ReturnJsonResponse(w, http.StatusMethodNotAllowed, handlers.MessageToJson(false, msg))
 		}
-	case match(p, "^([a-z]+)/\\d+$", &entity, &id):
+	case match(p, "^([a-z]+)/([0-9]+)+$", &entity, &id):
 		t := handlers.GetEntity(entity)
 
 		if t == nil {
 			msg := fmt.Sprintf("Entidade %s requisitada não definida", entity)
 			handlers.ReturnJsonResponse(w, http.StatusBadRequest, handlers.MessageToJson(false, msg))
 		} else if r.Method == http.MethodGet {
-			err = t.Get(w, id)
-		} else if r.Method == http.MethodDelete {
-			err = t.Delete(w, id)
+			err = t.Get(w, strconv.Itoa(id))
 		} else if r.Method == http.MethodPut {
 			err = t.Update(w, r, id)
 		} else {
 			msg := "Método e requisição solicitados são incompatíveis."
 			handlers.ReturnJsonResponse(w, http.StatusMethodNotAllowed, handlers.MessageToJson(false, msg))
 		}
-
+	case match(p, "^faixas/albuns/([0-9]+)+$", &id) && r.Method == http.MethodGet:
+		t := handlers.GetEntity("faixas")
+		err = t.Get(w, "cod_alb", strconv.Itoa(id))
 	default:
-		fmt.Println("não matchiou")
+		handlers.ReturnJsonResponse(w, http.StatusBadRequest, handlers.MessageToJson(false, "url inválida."))
 	}
 
 	time := time.Since(start).Microseconds()
@@ -77,7 +79,6 @@ func match(path, pattern string, vars ...interface{}) bool {
 	regex, err := regexp.Compile(pattern)
 
 	if err != nil {
-		println("nil Compile")
 		return false
 	}
 
@@ -94,12 +95,10 @@ func match(path, pattern string, vars ...interface{}) bool {
 		case *int:
 			n, err := strconv.Atoi(match)
 			if err != nil {
-				fmt.Println("type inv")
 				return false
 			}
 			*p = n
 		default:
-			println("type ummatch")
 			return false
 		}
 	}
